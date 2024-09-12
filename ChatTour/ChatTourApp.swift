a3dbb2172f12a17d52353d7db07ba0d0e7c67f92
Christian Wyglendowski <christian@dowski.com>
2024-09-12 16:27:15 -0400

    Initial project structure

---
+++ ChatTour/ChatTourApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatTourApp.swift
+//  ChatTour
+//
+//  Created by Christian Wyglendowski on 9/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ChatTourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
