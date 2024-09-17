e05a7073692e9ae3fdca84e05f0099fed77cb372
Christian Wyglendowski <christian@dowski.com>
2024-09-17 11:47:08 -0400

    Basic subscribe/mutation chat functionality

--- ChatTour/ContentView.swift
+++ ChatTour/ContentView.swift
@@ -5,20 +5,60 @@
 //  Created by Christian Wyglendowski on 9/12/24.
 //
 
+import Combine
+import ConvexMobile
 import SwiftUI
 
+let client = ConvexClient(deploymentUrl: "https://sensible-elephant-70.convex.cloud")
+
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+  @State private var messages: [Message] = []
+  @State private var cancellationHandle: Set<AnyCancellable> = []
+  @State private var outgoing: String = ""
+  var body: some View {
+    return VStack {
+      List {
+        ForEach(messages) { message in
+          Text(message.body)
         }
-        .padding()
+      }.task {
+        try! await client.subscribe(name: "messages:list")
+          .replaceError(with: [Message(id: "id", author: "None", body: "None")])
+          .receive(on: DispatchQueue.main)
+          .assign(to: \.messages, on: self)
+          .store(in: &cancellationHandle)
+      }
+      .padding()
+      HStack {
+        TextField("", text: $outgoing)
+          .border(.secondary)
+        Button(action: {
+          Task {
+            let x: Message? = try await client.mutation(
+              name: "messages:send", args: ["author": "iOS User", "body": outgoing])
+            outgoing = ""
+          }
+        }) {
+          Text("Send")
+        }
+
+      }
     }
+  }
+}
+
+struct Message: Identifiable, Decodable {
+  let id: String
+  let author: String
+  let body: String
+
+  enum CodingKeys: String, CodingKey {
+    case id = "_id"
+    case author
+    case body
+  }
 }
 
 #Preview {
-    ContentView()
+  ContentView()
 }
